65982be92cac7161511bb966cec016f6b3ed4353
legionscript <legionscript@legionscript.com>
2020-11-22 17:06:07 -0500

    Tutorial 6

--- customer/models.py
+++ customer/models.py
@@ -31,6 +31,7 @@ class OrderModel(models.Model):
     state = models.CharField(max_length=15, blank=True)
     zip_code = models.IntegerField(blank=True, null=True)
     is_paid = models.BooleanField(default=False)
+    is_shipped = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
--- restaurant/views.py
+++ restaurant/views.py
@@ -12,19 +12,47 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
         orders = OrderModel.objects.filter(
             created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
 
-        # loop through the orders and add the price value
+        # loop through the orders and add the price value, check if order is not shipped
+        unshipped_orders = []
         total_revenue = 0
         for order in orders:
             total_revenue += order.price
 
+            if not order.is_shipped:
+                unshipped_orders.append(order)
+
         # pass total number of orders and total revenue into template
         context = {
-            'orders': orders,
+            'orders': unshipped_orders,
             'total_revenue': total_revenue,
             'total_orders': len(orders)
         }
 
         return render(request, 'restaurant/dashboard.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+
+
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        order.is_shipped = True
+        order.save()
+
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
 
     def test_func(self):
         return self.request.user.groups.filter(name='Staff').exists()
